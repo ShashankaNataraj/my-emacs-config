9d43125d43fbe34d5f9078e868898ab059b42c4a
Shashanka Nataraj <shasn@cisco.com>
2018-06-05 15:05:03 +0530

    Added telephone line customization

--- mode-line.el
+++ mode-line.el
@@ -1,2 +1,5 @@
-(require 'telephone-line)
+(setq telephone-line-lhs
+        '((evil     . (telephone-line-evil-tag-segment))))
+(setq telephone-line-rhs
+        '((accent   . (telephone-line-vc-segment))))
 (telephone-line-mode 1)
