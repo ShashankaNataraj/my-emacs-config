588492de59f7a7cdf8bb7b594fbc5b8096b5c694
Shashanka Nataraj <shasn@cisco.com>
2018-06-20 10:34:10 +0530

    Switched to milky postmans powerline

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # my-emacs-config
-MY emacs config. Not yours. Mine.
+My emacs config is my magnum opus. I have spent thousands of hours searching for the ideal editor. I have tried Vim, VSCode, Atom, Webstorm, oni, Kakoune and everything in between and settled at using emacs.
+
+This config repo is a labour of love. Use it at your own peril.
--- mode-line.el
+++ mode-line.el
@@ -1,5 +1,9 @@
-(setq telephone-line-lhs
-        '((evil     . (telephone-line-evil-tag-segment))))
-(setq telephone-line-rhs
-        '((accent   . (telephone-line-vc-segment))))
-(telephone-line-mode 1)
+;(setq telephone-line-lhs
+;        '((evil     . (telephone-line-evil-tag-segment))))
+;(setq telephone-line-rhs
+;        '((accent   . (telephone-line-vc-segment))))
+;;(telephone-line-mode 1)
+(require 'powerline)
+(powerline-default-theme)
+(require 'airline-themes)
+(load-theme 'airline-simple)
