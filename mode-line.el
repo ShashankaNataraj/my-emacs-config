f87421b2d9e3994753f9b8b8c73da28cc533131d
Shashanka Nataraj <shasn@cisco.com>
2018-06-20 10:35:03 +0530

    Removed useless telephone defs

--- mode-line.el
+++ mode-line.el
@@ -1,8 +1,3 @@
-;(setq telephone-line-lhs
-;        '((evil     . (telephone-line-evil-tag-segment))))
-;(setq telephone-line-rhs
-;        '((accent   . (telephone-line-vc-segment))))
-;;(telephone-line-mode 1)
 (require 'powerline)
 (powerline-default-theme)
 (require 'airline-themes)
